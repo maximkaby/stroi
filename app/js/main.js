a4bf2f9052fb2408a41a1fa012cd0e9b64907d13
Oleg.Buyalski <zidabah@mail.ru>
2017-12-07 01:34:24 +0300

    add blocks swiper and services

--- app/js/main.js
+++ app/js/main.js
@@ -67,4 +67,22 @@ Materialize.scrollFire(options);
     else {
     document.getElementById('input-submit').value = 'Отправить сообщение';
   }
+
+
+
+  var swiper = new Swiper('.swiper-container', {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    slidesPerGroup: 1,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
 });
